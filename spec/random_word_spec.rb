559574d77c9161b67469bbd54594fd6a1eb68a09
Woulei <woulei@me.com>
2017-03-26 16:37:12 +0200

    Wrote random_word tests

--- spec/random_word_spec.rb
+++ spec/random_word_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe RandomWord do
       expect(random_word.positions_for(characters: ["e"])).to eq("_ _ _ e _ _ e _")
     end
   end
+  describe "all_letters? works correctly" do
+    before do
+      allow(random_word).to receive(:word).and_return("homebrew")
+    end
+
+    it "Return true is all letters have been guessed" do
+      letters = ["h","o","m","e","b","r","w"]
+      expect(random_word.all_letters?(letters)).to eq(true)
+      expect(random_word.all_letters?(letters)).not_to eq(false)
+    end
+    it "Return false if some letters have not been guessed" do
+      letters = ["h","o","m","e","b","r"]
+      expect(random_word.all_letters?(letters)).not_to eq(true)
+      expect(random_word.all_letters?(letters)).to eq(false)
+    end
+  end
 end
